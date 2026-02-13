3353f3856c550b9eda48da10a2798495a14c67af
Zoha <160307821+Zoha-Khan123@users.noreply.github.com>
2026-02-14 01:09:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+// var age = 18;
+
+// if(age>=18){
+//     console.log("You can vote");
+    
+// }
+
+// var num = prompt("Enter any number")
+
+
+// if(num % 2 === 0){
+//     console.log("Even number");
+    
+// }
+
+
+// var password = prompt("Enter your password")
+
+
+// if(password === "12345"){
+//     console.log("Login successsful");
+    
+// }
+
+
+
+// var num = 123
+// if(num !== 123){
+//     console.log("Pass");
+// }
+
+
+// var city = prompt("Enter your favourite city")
+// if(city !== "Karachi"){
+//     console.log("You are not from Karachi");
+    
+// }
+
+
+// var email = prompt("Enter your email")
+// if(email !== "zohakhan@gmail.com"){
+//     console.log("Error : User not found");
+    
+// }
+
+
+
+// var username = prompt("Enter your username")
+// if(username === "Zoha"){
+//     console.log("Welcome Zoha!");
+// }else{
+//     console.log("User not found");
+// }
+
+// var shoppingAmount = +prompt("Enter your shopping amount")
+// if(shoppingAmount >= 5000){
+//     console.log("You can get 20% off");
+// }else{
+//     console.log("No discount availabe");
+    
+// }
+
+
+
+
+// var dressColor = prompt("Which color dress do you want");
+
+// if(dressColor === "Red"){
+//     console.log("Buy this red color dress");
+// }else if(dressColor === "Blue"){
+//     console.log("Buy this blue color dress");
+// }else if(dressColor == "Yellow"){
+//     console.log("Buy this yellow color dress");
+// }else{
+//     console.log("Buy any color dress");
+// }
+
+
+
+// var battery = Number(prompt("Enter battery percentage"));
+
+// if(battery > 80){
+//     console.log("Battery Full 🔋");
+// } 
+// else if(battery > 30){
+//     console.log("Battery Medium ⚡");
+// } 
+// else {
+//     console.log("Low Battery! Charge Now 🔴");
+// }
+
+
+
+// var marks = prompt("Enter your marks")
+// if(marks >= 90){
+//     console.log("Excellent");
+// }else if(marks >= 70){
+//     console.log("Good");
+// }else if(marks >= 50){
+//     console.log("Average");
+// }else{
+//     console.log("Fail");
+// }
+
+
+// var age = prompt("Enter your age")
+// var permissionSlip = prompt("You have permission slip or not only enter (true or false)")
+
+
+
+// if(age > 18 && permissionSlip === "true"){
+//     console.log("You can go the trip");
+// }else{
+//     console.log("You can not go");
+// }
+
+
+
+
+// var email = prompt("Enter email")
+// var password = prompt("Enter password")
+// if(email === "zohakhan@gmail.com" && password === "12345"){
+//     console.log("Login successfully");
+// }else{
+//     console.log("Login fail");
+// }
+
+
+// var day = prompt("Enter day")
+// if(day === "Sunday" || day === "Saturday"){
+//     console.log("Weekend day");
+// }else{
+//     console.log("No weekend day"); 
+// }
+
+
+// var isRaining = true
+// if(!isRaining){
+//     console.log("Bahaar jaw");
+// }else{
+//     console.log("Ghar ma raho");
+// }
